4947f46b26ef6ea1839b06b8d579e7a23d2d5b5a
AbdumalikovIkboljon <abdumalikovikboljon0102@gmail.com>
2024-10-17 09:21:58 +0900

    create storage for saving data of project

--- constants/index.js
+++ constants/index.js
@@ -1,3 +1,192 @@
+export const companyInfo = [
+  {
+    id: 1,
+    image: '',
+    name: 'Company',
+    title: 'Who we are',
+    description: '맞춤형 소프트웨어 개발, 이커머스 플랫폼 구축, AI 기반 솔루션, ERP 시스템, 모바일 및 웹 애플리케이션 개발 등 Firgate는 혁신적인 소프트웨어 솔루션을 제공하는 IT 전문 기업입니다.'
+  },
+  {
+    id: 2,
+    image: '',
+    name: 'Team',
+    title: 'What we do',
+    description: '우리는 고객의 요구에 맞춘 최적의 솔루션을 제공하며, 다양한 산업 분야에서 풍부한 경험을 쌓아왔습니다. 우리의 목표는 기술을 통해 고객의 비즈니스를 성장시키고, 효율성을 극대화하는 것입니다.'
+  }
+]
+
+
+export const projectDetails = [
+  {
+    title: '이커머스 솔루션',
+    details: [
+      {
+        title: ' 컴포트랩',
+        description: '맞춤 큐레이션 속옷 이커머스 플랫폼을 개발하여, 사용자 맞춤 추천 시스템을 도입했습니다. 이로 인해 매출이 20% 증가했습니다.',
+      },
+      {
+        title: ' 임펙스필',
+        description: '온라인 약국 커머스를 구축하여, 고객이 쉽고 빠르게 약품을 구매할 수 있는 사용자 친화적인 인터페이스를 제공했습니다.',
+      },
+    ]
+  },
+  {
+    title: 'AI 및 데이터 솔루션',
+    details: [
+      {
+        title: '노트미',
+        description: ' AI 기반의 모의 면접 연습 플랫폼으로, 사용자 피드백을 통해 면접 성공률을 15% 향상시켰습니다.'
+      },
+      {
+        title: '뇌 영상 분석 플랫폼',
+        description: 'MRI 영상을 분석하여 병을 진단하는 AI 기반 서비스 플랫폼을 구축했습니다.'
+      }
+    ]
+  },
+  {
+    title: '전문직 및 커뮤니티 플랫폼',
+    details: [
+      {
+        title: '더로이어즈 / 디어카운턴츠 / 더브이씨',
+        description: ' 전문직 종사자를 위한 커뮤니티 플랫폼을 개발하여, 업계 전문가들이 서로의 경험을 공유할 수 있는 공간을 마련했습니다.',        
+      },
+      {
+        title: '모두의 변호사',
+        description: '변호사를 위한 취업 정보 플랫폼으로, 사용자 맞춤형 공고 알림 기능을 구현했습니다.',
+      }
+    ]
+  },
+  {
+    title: '모바일 및 웹 애플리케이션',
+    details: [
+      {
+        title: 'DCS 앱 및 AP 디바이스',
+        description: '치매 환자를 위한 모바일 앱과 고정형 태블릿 디바이스를 개발하여, 환자 관리의 효율성을 높였습니다.'
+      },
+      {
+        title: '김포시청 결식아동 지원 서비스',
+        description: '지류 쿠폰을 모바일로 전환하여, 결식아동 지원 서비스를 디지털화했습니다.'
+      }
+    ]
+  },
+  {
+    title: 'ERP 솔루션',
+    details: [
+      {
+        title: '태권매니저 (태권도장 전용 ERP 시스템)',
+        description: '태권도장의 운영을 위한 맞춤형 ERP 시스템으로, 수강생 관리, 출석 체크, 회계 관리 등 도장의 모든 운영을 하나의 플랫폼에서 효율적으로 처리할 수 있도록 설계되었습니다.'
+      },
+      {
+        title: '인슈어즈 (개인 보험설계사 전용 ERP)',
+        description: '보험설계사를 위한 ERP 시스템으로, 고객 관리, 계약서 작성, 일정 관리 등 다양한 기능을 통합하여 설계했습니다.'
+      }
+    ]
+  }
+]
+
+
+export const services = [
+  {
+    icon: '',
+    title: '컴포트랩',
+    description: '메인화면 캡쳐',
+  },
+  {
+    icon: '',
+    title: '노트미',
+    description: 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life',
+  },
+  {
+    icon: '',
+    title: '뇌 영상 분석 플랫폼',
+    description: 'Blind texts it is an almost unorthographic life One day however a small line of blind text by the name',
+  },
+  {
+    icon: '',
+    title: '모두의 변호사',
+    description: 'Blind texts it is an almost unorthographic life One day however a small line of blind text by the name',
+  },
+  {
+    icon: '',
+    title: 'Consulting',
+    description: 'Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.',
+  },
+  {
+    icon: '',
+    title: 'Events',
+    description: 'Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life',
+  },
+]
+
+export const teamMembers = [
+  {
+    id: 1,
+    image: '',
+    name: 'James Kim',
+    role: 'CEO',
+  },
+  {
+    id: 2,
+    image: '',
+    name: 'Bianca Lee',
+    role: 'CTO',
+  },
+  {
+    id: 3,
+    image: '',
+    name: 'Amanda Peterson',
+    role: 'Project Manager',
+  },
+]
+
+export const contactInfo = [
+  {
+    id: 1,
+    image: '',
+    name: 'Amanda Peterson',
+    role: 'CEO',
+    email: 'ceo@business.com',
+    phoneNumber: '01 (650) 658 6823',
+  },
+  {
+    id: 2,
+    image: '',
+    name: 'Jason Smith',
+    role: 'CTO',
+    email: 'cto@business.com',
+    phoneNumber: '01 (650) 658 6824',
+  },
+  {
+    id: 3,
+    image: '',
+    name: 'Michael Fassbender',
+    role: 'Design Director',
+    email: 'design@business.com',
+    phoneNumber: '01 (650) 658 6825 ',
+  }
+]
+
+export const teamProjects = [
+  {
+    id: 1,
+    image: '',
+    name: 'Project #1',
+    description: 'Art direction',
+  },
+  {
+    id: 2,
+    image: '',
+    name: 'Project #2',
+    description: 'Product Design',
+  },
+  {
+    id: 3,
+    image: '',
+    name: 'Project #3',
+    description: 'Animation',
+  },
+]
+
 export const footerLinks = [
   {
     title: "About",
